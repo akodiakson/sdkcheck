2f845a9fdf4d73d78b473c3ac7e1a2a8b1db5a1e
A. C. Erickson <andrewericksonisu@gmail.com>
2015-12-11 12:57:22 -0600

    -- Introduced static, OO, singleton variants

---
+++ app/src/test/java/com/akodiakson/sdk/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.akodiakson.sdk;
+
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    public void isAtLeastMarshmallow() throws Exception {
+        //TODO -- First test.
+    }
+}
