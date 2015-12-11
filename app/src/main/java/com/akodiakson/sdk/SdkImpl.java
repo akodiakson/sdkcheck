d3a8bb2be3f45818417a17caa0f2183c1ab1ac08
A. C. Erickson <andrewericksonisu@gmail.com>
2015-12-11 13:38:30 -0600

    -- Added open source declarations for Issue #1

--- app/src/main/java/com/akodiakson/sdk/SdkImpl.java
+++ app/src/main/java/com/akodiakson/sdk/SdkImpl.java
@@ -2,20 +2,25 @@ package com.akodiakson.sdk;
 
 import android.os.Build;
 
+/*
+ * Copyright (C) 2015 Andrew Erickson (akodiakson)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class SdkImpl implements Sdk {
 
-
-    public void example(){
-        Sdk sdk = new SdkImpl();
-        if(sdk.isAtLeastMarshmallow()){
-            System.out.println("I'm fluffy");
-        } else if(sdk.isAtLeastLollipop()){
-            System.out.println("I'm sticky");
-        } else if(sdk.isAtLeastKitKat()){
-            System.out.println("I'm crunchy");
-        }
-    }
-    public int getVersion() {
+public int getVersion() {
         return android.os.Build.VERSION.SDK_INT;
     }
 
